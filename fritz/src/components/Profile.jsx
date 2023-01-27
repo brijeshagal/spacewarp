0762bfd5306b476f0a0626489eca4539903f128a
aradhya-10 <202051034@iiitvadodara.ac.in>
2023-01-27 20:21:00 +0530

    Add frontend

---
+++ fritz/src/components/Profile.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="container bg-red-500 bg-opacity-10 z-50">
+      <div className="container text-white  mx-auto p-20 lg:w-5/6 flex flex-row justify-center bg-pink-600 bg-opacity-10 z-[100]">
+        <div className="flex flex-col justfy-center">
+          <div className="flex flex-row justify-center">
+            <img src="https://img.icons8.com/ultraviolet/512/duolingo-logo.png" className="h-48"/>
+            <div className="container px-8 py-4"> Hello</div>
+          </div>
+          <div className="container">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem,
+              fuga qui labore tempora unde quam fugiat, omnis aut iusto delectus
+              at? Corporis quod debitis dolor optio? Officiis voluptatem
+              consectetur possimus! Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Minus eveniet possimus at delectus, consequatur,
+              voluptates sequi facere itaque voluptate, quidem dignissimos. Quos
+              expedita dolore vel quibusdam, libero odio fugiat placeat.
+              Consectetur provident adipisci, animi voluptatum eius, ducimus
+              enim soluta ut exercitationem excepturi facilis, cupiditate nulla
+              nesciunt. Voluptates quae quam ad culpa explicabo, atque, veniam
+              eum accusamus cupiditate cumque officia dolorum. Aspernatur, sed?
+              Facere temporibus asperiores necessitatibus. Commodi officia vero
+              adipisci facilis et voluptatem, dolorum voluptatum impedit
+              doloremque fuga nam distinctio cupiditate fugiat animi nobis dolor
+              necessitatibus ut, accusantium dolore. Voluptate? Deserunt
+              doloribus illo numquam vel eveniet? Quibusdam iure soluta
+              temporibus, error numquam deleniti quos doloremque officiis earum
+              qui. Facere neque eius alias molestias quae dolorem perferendis
+              voluptatum rerum tenetur cupiditate! Nesciunt labore corporis
+              suscipit atque dolorem fuga corrupti autem rem. Quia voluptate
+              itaque deserunt enim saepe tempore in, provident maxime
+              necessitatibus animi similique ab rerum voluptas fugit quaerat
+              cumque natus? Rerum veniam eveniet fuga in quia quos laboriosam
+              dolore iusto eos, commodi soluta officiis maxime odit accusantium
+              vel culpa consequuntur omnis earum architecto suscipit ab ut
+              molestiae! Eveniet, labore voluptates. Nesciunt quidem dolorem
+              eaque numquam pariatur iure dicta inventore tempore dolor saepe
+              dolore possimus libero deserunt, sunt dolores temporibus unde
+              totam necessitatibus. Quas, sapiente suscipit. Aliquam tempore
+              voluptatibus veniam dolorem.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
